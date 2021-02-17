20af9ed4897946a11f74076d955e95993ebf1bcf
rkey1997 <kopytov_rodion@mail.ru>
2021-02-17 12:42:21 +0400

    fff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Wrapper from './components/Wrapper'
 
 function App() {
-  return (
-    <div className="App">
-      <Wrapper/>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        Wrapper / >
+        <
+        /div>
+    );
 }
 
 export default App;
